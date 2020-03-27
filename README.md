e010ad2c39425ccd99861ffa1be2bc7624a9337f
8bitago <eightbitago@gmail.com>
2020-03-27 11:12:36 +0100

    fix readme

--- README.md
+++ README.md
@@ -7,8 +7,36 @@ This is my pet-project - simple task manager. Tech stack: React, Redux, React Ro
 To run this project you need:
 
 ```sh
-$ yarn inastall && yarn start
+$ yarn inastall
 ```
+
+change config in ./server/config/keys.ts
+
+```js
+// Google credential
+export const GOOGLE = {
+  CLIENT_ID: "your google id",
+  CLIENT_SECRET: "your google secret",
+  CALLBACK_URL: "you google url"
+};
+
+// Setup mongodb connection
+export const MONGODB = {
+  MONGODB_URI: `you mongodb url`,
+};
+
+export const SESSION = {
+  COOKIE_KEY: "thisappisawesome"
+};
+```
+
+then need run:
+```sh
+yarn development
+```
+
+ps: prod build hasn't completed yet
+
 
 ## License
 
--- server/config/keys.ts
+++ server/config/keys.ts
@@ -1,15 +1,13 @@
+// Google credential
 export const GOOGLE = {
-  CLIENT_ID: "26125392638-ch7s2ov36ao762h4b5mqukn1n2rqhimp.apps.googleusercontent.com",
-  CLIENT_SECRET: "7iLOgsCX_Ke1BF4FvxC_pxFi",
-  CALLBACK_URL: "http://localhost:5000/api/auth/google/callback"
+  CLIENT_ID: "your google id",
+  CLIENT_SECRET: "your google secret",
+  CALLBACK_URL: "you google url"
 };
 
-export const DB_USER = "SOME USER";
-export const DB_PASSWORD = "SOME PASSWPORD";
-
+// Setup mongodb connection
 export const MONGODB = {
-  MONGODB_URI: `mongodb://localhost:27017/plexideas-todo`,
-  S_MONGODB_URI: `mongodb://${DB_USER}:${DB_PASSWORD}@ds<SOME_DOMAIN>.mlab.com:<PORT>/<PROJECT_NAME>`
+  MONGODB_URI: `you mongodb url`,
 };
 
 export const SESSION = {
